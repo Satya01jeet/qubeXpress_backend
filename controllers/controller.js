4ebd178f8deb8ed93e076071c66046e9de86fd99
Satya <sausatyajeet100@gmail.com>
2024-09-09 11:48:01 +0530

    complete controller.js

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,5 +1,26 @@
+const { response } = require('express');
+const model = require('../models/logistics');
+const csv = require('csvtojson');
+
 const importData = async (req,res) => {
     try{
+        var data = [];
+        csv().fromFile(req.file.path).then(async (response) => {
+            console.log(response);
+            for(var x=0; x<response.length; x++){
+                data.push({
+                    customer_id: response[x].customer_id,
+                    tracking_id: response[x].tracking_id,
+                    courier_partner: response[x].courier_partner,
+                    awb_number: response[x].awb_number,
+                    order_shipped_date: response[x].order_shipped_date,
+                    expected_delivery_date: response[x].expected_delivery_date,
+                    status: response[x].status,
+                });
+            }
+            await model.insertMany(data);
+        })
+
         res.send({
             status: 200,
             success: true,
